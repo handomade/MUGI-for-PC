107750758c601e1b6a6115debfa372769a1a7327
Hando <hk6dh1031@gmail.com>
2026-03-14 05:45:12 +0900

    buildフォルダ

---
+++ build/CMakeFiles/mugi.dir/src/game/actpazle_password.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/mugi.dir/src/game/actpazle_password.c.obj: \
+ C:\Users\hk6dh\Downloads\mugi_sdl2\src\game\actpazle_password.c \
+ C:/Users/hk6dh/Downloads/mugi_sdl2/include/msxgl_types.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/msys64/ucrt64/include/stdint.h C:/msys64/ucrt64/include/crtdefs.h \
+ C:/msys64/ucrt64/include/corecrt.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/vadefs.h \
+ C:/msys64/ucrt64/include/sdks/_mingw_ddk.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/msys64/ucrt64/include/stddef.h
