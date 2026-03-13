fe62e2b8919eb143451e3b2e8c9ccdfe5756b80b
Hando <hk6dh1031@gmail.com>
2026-03-14 05:46:15 +0900

    buildフォルダ残り

---
+++ build/CMakeFiles/mugi.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mugi.dir/src/game/actpazle.c.obj"
+  "CMakeFiles/mugi.dir/src/game/actpazle.c.obj.d"
+  "CMakeFiles/mugi.dir/src/game/actpazle_password.c.obj"
+  "CMakeFiles/mugi.dir/src/game/actpazle_password.c.obj.d"
+  "CMakeFiles/mugi.dir/src/hal/audio_sdl2.c.obj"
+  "CMakeFiles/mugi.dir/src/hal/audio_sdl2.c.obj.d"
+  "CMakeFiles/mugi.dir/src/hal/input_sdl2.c.obj"
+  "CMakeFiles/mugi.dir/src/hal/input_sdl2.c.obj.d"
+  "CMakeFiles/mugi.dir/src/hal/vdp_sdl2.c.obj"
+  "CMakeFiles/mugi.dir/src/hal/vdp_sdl2.c.obj.d"
+  "CMakeFiles/mugi.dir/src/main.c.obj"
+  "CMakeFiles/mugi.dir/src/main.c.obj.d"
+  "libmugi.dll.a"
+  "mugi.exe"
+  "mugi.exe.manifest"
+  "mugi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mugi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
